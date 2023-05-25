4bab49a8dae3a65c8c1139a01d8dcdf5f6087796
To Chin Yu <stevenytc@gmail.com>
2023-05-24 22:23:43 -0700

    Expose time parameters

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ import streamlit as st
 st.set_page_config(page_title="Stock Trend Visualizer", page_icon='📈', layout="centered", initial_sidebar_state="auto", menu_items=None)
 
 ticker=st.text_input('Ticker',value='AAPL')
+time_horizon=st.text_input('Time Horizon (Days)',value='60')
+days_back=st.text_input('Time Lookback (Days)',value='600')
 s = Stock(ticker) # stock ticker
-time_horizon = 90 # number of days to look ahead
+time_horizon = int(time_horizon) # number of days to look ahead
 
 
 # Prophet
-df = s.historical(days_back=600, frequency='d')
+df = s.historical(days_back=int(days_back), frequency='d')
 df['ds'] = pd.to_datetime(df['Date'])
 df['volume'] = np.log(df.Volume)
 df['y'] = df['Adj Close']
