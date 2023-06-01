72b769dd6018319b998a62f6c089b1a68c93da9c
To Chin Yu <stevenytc@gmail.com>
2023-05-31 21:03:50 -0700

    add options term structure

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pandas as pd
 import numpy as np
+from scipy.interpolate import UnivariateSpline
+from joblib import Parallel,delayed
+import plotly.express as px
+from functools import partial
 
 import prophet as ph
-from wallstreet import Stock
+from wallstreet import Stock, Call, Put
 
 import streamlit as st
 
 st.set_page_config(page_title="Stock Trend Visualizer", page_icon='📈', layout="centered", initial_sidebar_state="auto", menu_items=None)
 
-ticker=st.text_input('Ticker',value='AAPL')
-time_horizon=st.text_input('Time Horizon (Days)',value='60')
-days_back=st.text_input('Time Lookback (Days)',value='600')
+c1,c2,c3 = st.columns(3)
+with c1:
+    ticker=st.text_input('Ticker',value='AAPL')
+with c2:
+    time_horizon=st.text_input('Time Horizon (Days)',value='60')
+with c3:
+    days_back=st.text_input('Time Lookback (Days)',value='600')
 s = Stock(ticker) # stock ticker
 time_horizon = int(time_horizon) # number of days to look ahead
 
@@ -29,4 +37,33 @@ forecast = m.predict(future)
 fig=m.plot(forecast)
 st.pyplot(fig)
 
-st.dataframe(forecast.tail(time_horizon))
+# st.dataframe(forecast.tail(time_horizon))
+
+# Option expectation structure
+expirations = Call(ticker).expirations
+def getprice(strike,expiration):
+  c = Call(ticker,strike=strike, d=expiration.day, m=expiration.month, y=expiration.year)
+  p = Put(ticker,strike=strike, d=expiration.day, m=expiration.month, y=expiration.year)
+  return {'Strike':strike,'CallPrice':(c.ask+c.bid)/2,'PutPrice':(p.ask+p.bid)/2}
+
+def getimpliedprice(ticker,expiration):
+  s0 = Stock(ticker).price
+  c = Call(ticker, d=expiration.day, m=expiration.month, y=expiration.year)
+  p = Put(ticker, d=expiration.day, m=expiration.month, y=expiration.year)
+  strikes = np.asarray(list(set(c.strikes) & set(p.strikes)))
+  strikes = strikes[(strikes>0.85*s0) & (strikes < 1.15*s0)]
+  entries = Parallel(n_jobs=-1)(delayed(partial(getprice,expiration=expiration))(s) for s in strikes)
+  df=pd.DataFrame(entries).set_index('Strike').sort_index()
+
+  spl_callprice = UnivariateSpline(df.index,df.CallPrice,k=4)
+  spl_putprice = UnivariateSpline(df.index,df.PutPrice,k=4)
+  w_call=np.maximum(0,spl_callprice.derivative(2)(df.index))
+  s_call=np.sum(w_call*df.index)/np.sum(w_call)
+  w_put=np.maximum(0,spl_putprice.derivative(2)(df.index))
+  s_put=np.sum(w_put*df.index)/np.sum(w_put)
+
+  return {'Date':expiration,'Call Implied':s_call,'Put Implied': s_put}
+
+df_opt = pd.DataFrame([getimpliedprice(ticker,pd.to_datetime(exp,dayfirst=True)) for exp in expirations[:3]]).set_index('Date')
+fig_opt = px.line(df_opt)
+st.plotly_chart(fig_opt)
